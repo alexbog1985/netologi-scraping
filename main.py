b7b1707188d492f1eea940f51d04a5f5040eca86
Aleksey Bogdanov <aleksey@bogdanov77.ru>
2024-04-24 16:38:07 +0300

    hw v1 complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from bs4 import BeautifulSoup
 import requests
+import json
+from bs4 import BeautifulSoup
 from fake_headers import Headers
 
 header = Headers(browser="chrome",
@@ -10,8 +11,7 @@ header = Headers(browser="chrome",
 params = {'L_save_area': 'true',
           'text': '(python and django) or (python and flask)',
           'excluded_text': '',
-          'area': '1',
-          'area': '2',
+          'area': ['1', '2'],
           'salary': '',
           'currency_code': 'USD',
           'experience': 'doesNotMatter',
@@ -23,14 +23,14 @@ params = {'L_save_area': 'true',
           }
 
 
-def get_pages(pages=4):
+def get_pages(pages=3):
     div_tags_lst = []
     for page in range(pages):
         response = requests.get('https://spb.hh.ru/search/vacancy', headers=header, params=params)
+        print(response.url)
         if response.status_code == 200:
             bs = BeautifulSoup(response.text, 'lxml')
             params['page'] = page
-            print(bs.find('h1'))
             tags = bs.find_all('div', class_='vacancy-serp-item-body') 
             if tags:
                 div_tags_lst.append(tags)
@@ -43,15 +43,22 @@ def scrap(div_tags_lst):
     res = []
     for div_tags in div_tags_lst:
         for tag in div_tags:
-            link = tag.find('a', class_='bloko-link')
-            salary = tag.find('span', class_='bloko-header-section-2')
-            company = tag.find('div', class_='vacancy-serp-item__meta-info-company')
-            print(company.text)
-            print(salary.text if salary else 'Зарлата не указана')
-            print(link['href'])
-            res.append(tag.text)
+            link = tag.find('a', class_='bloko-link').text
+            pre_salary = tag.find('span', class_='bloko-header-section-2')
+            salary = pre_salary.text if pre_salary else 'Зарплата не указана'
+            company = tag.find('div', class_='vacancy-serp-item__meta-info-company').text
+            pre_city = tag.find('div', attrs={'data-qa': 'vacancy-serp__vacancy-address'}).text
+            city = pre_city.split(',')[0]
+            res.append({'link': link, 'salary': salary.replace(u'\u202f', ' '), 'company': company.replace(u'\xa0', ' '), 'city': city})
+            print(res)
     print(len(res))
+    return res
+
+def save_json(data):
+    with open('vacancies.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == '__main__':
-        scrap(get_pages())
+        data = scrap(get_pages())
+        save_json(data)
